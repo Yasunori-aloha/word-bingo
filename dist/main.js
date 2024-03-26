d2e3fedf5e10ef8f5d99d0f54946bf9d5ff0de5c
Yasunori <zumigoku@gmail.com>
2024-03-26 16:37:10 +0000

    標準入力処理の実装。 ※TypeScriptの型エラーが発生するため、nodeの型定義を導入。

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,27 @@
 "use strict";
+/**
+ * メイン処理
+ * ここで『ワードビンゴ』の処理を実施する。
+ * @param inputs 標準入力データの配列(改行区切り)
+ */
+const main = (inputs) => {};
+/**
+ * 標準入力処理
+ */
+const inputProcess = () => {
+  const inputs = [];
+  // 標準入力ストリームを開始
+  process.stdin.resume();
+  // 標準入力で受け取ったデータをUTF-8でエンコード
+  process.stdin.setEncoding("utf8");
+  // 標準入出力を使用したインタフェースを作成
+  const reader = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  // 標準入力の改行ごとにinputs配列に行のデータを格納
+  reader.on("line", (line) => inputs.push(line));
+  // 標準入力が完了したらmain関数を実行
+  reader.on("close", () => main(inputs));
+};
+inputProcess();
---
+++ src/main.ts
@@ -0,0 +1,33 @@
+/**
+ * メイン処理
+ * ここで『ワードビンゴ』の処理を実施する。
+ * @param inputs 標準入力データの配列(改行区切り)
+ */
+const main = (inputs: string[]) => {};
+
+/**
+ * 標準入力処理
+ */
+const inputProcess = (): void => {
+  const inputs: Array<string> = [];
+
+  // 標準入力ストリームを開始
+  process.stdin.resume();
+
+  // 標準入力で受け取ったデータをUTF-8でエンコード
+  process.stdin.setEncoding("utf8");
+
+  // 標準入出力を使用したインタフェースを作成
+  const reader: any = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  // 標準入力の改行ごとにinputs配列に行のデータを格納
+  reader.on("line", (line: string) => inputs.push(line));
+
+  // 標準入力が完了したらmain関数を実行
+  reader.on("close", () => main(inputs));
+};
+
+inputProcess();
